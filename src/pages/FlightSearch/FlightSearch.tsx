6c975d4ddbd91101ab80fb2151b9211d2cb69242
Tolunay Tezcan <tolunay.tzcn7@gmail.com>
2024-10-09 23:05:23 +0300

    Add FlightSearch page with styles

---
+++ src/pages/FlightSearch/FlightSearch.tsx
@@ -0,0 +1,12 @@
+import Header from '@App/layout/Header/Header';
+import styles from './FlightSearch.module.scss';
+
+const FlightSearch = () => {
+	return (
+		<div className={styles.wrapper}>
+			<Header />
+		</div>
+	);
+};
+
+export default FlightSearch;
