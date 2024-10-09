f154ad487f6ca0ae9e0d2fa4413cd3397bec0c84
Tolunay Tezcan <tolunay.tzcn7@gmail.com>
2024-10-09 23:06:01 +0300

    Add react router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import styles from '@App/App.module.scss';
 import FlightListing from '@App/pages/FlightListing';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import FlightSearch from './pages/FlightSearch';
+import Result from './pages/Result';
+
+import './styles/globals.scss';
 
 const App = () => (
 	<div className={styles.wrapper}>
-		<FlightListing />
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<FlightListing />} />
+				<Route path='/search' element={<FlightSearch />} />
+				<Route path='/result' element={<Result status='success' />} />
+			</Routes>
+		</BrowserRouter>
 	</div>
 );
 
