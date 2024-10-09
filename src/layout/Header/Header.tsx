db5518b7016f12554bbd83953f89f3700ce0e1e0
Tolunay Tezcan <tolunay.tzcn7@gmail.com>
2024-10-09 23:04:38 +0300

    Add header component and global styles

---
+++ src/layout/Header/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './header.module.scss';
+
+interface HeaderProps {
+	isSearchPage?: boolean;
+}
+
+const Header: React.FC<HeaderProps> = ({ isSearchPage }) => (
+	<header>
+		<div
+			className={`${styles.header_container} ${isSearchPage ? styles.searchPage : ''}`}
+		>
+			<span>turkishairlines.com</span>
+			<span>
+				search<b>Flight Challenge</b>
+			</span>
+		</div>
+	</header>
+);
+
+export default Header;
